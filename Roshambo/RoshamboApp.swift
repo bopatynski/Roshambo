dee234b4eca16383bdbf9515c074ba6dd8f6d520
bopatynski <bopatynski@gmail.com>
2022-04-26 20:06:34 -0400

    Initial Commit

---
+++ Roshambo/RoshamboApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoshamboApp.swift
+//  Roshambo
+//
+//  Created by Bogdan Patynski on 2022-04-26.
+//
+
+import SwiftUI
+
+@main
+struct RoshamboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
